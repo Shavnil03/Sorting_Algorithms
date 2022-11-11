122bc776bf73008f6efdaedf0cdf2d404040f72b
shabneel kumar <105269133+Shavnil03@users.noreply.github.com>
2022-11-12 09:28:10 +1200

    Update Merge_Sort.java

--- Merge_Sort.java
+++ Merge_Sort.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Merge_sort;
+
+/*
+   Shabneel Kumar
+   S11184954
+   This is a fully functional genric Merge sort Algorithm code in java
+*/
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
-/**
- *
- * @author user
- */
+ 
 public class Merge_Sort {
 
     public int counter_M;
